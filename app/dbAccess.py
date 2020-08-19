b2da4f4e4bf052b157ed8f5645bfa824ce7a788b
joepasquale <jpasquale@umass.edu>
2020-08-19 08:50:36 -0400

    update - 08/19 (the security update!)
    updated dom so that 'reset query' button is only shown on query page. it doesn't really change much, as doing so before would just leave you on the default database, but i suppose it better locks down the application
    
    moved default connection (for fetching list of dbs at home) into the loadDBList() method. trying to minimize active connections, as the user base in mind uses a lot of mobile computers (microsoft surface), so we don't want those to leave the server hanging
    
    added "injectionAlert()" to detect key words in a query, which are operations that should not be executed by this tool at all. this is a read only tool, so there's no need to execute anything that modifies the data in the database.

--- app/dbAccess.py
+++ app/dbAccess.py
@@ -8,12 +8,8 @@ import csv
 import pyodbc
 
 #default db connection
-conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};'
-                      'SERVER=*;'
-                      'DATABASE=*;'
-                      'uid=*;'
-                      'pwd=*;')
-
+conn = ""
+userConn = ""
 #used to store data for csv
 curData = []
 curTable = ""
@@ -24,6 +20,13 @@ curColumns = []
 #params: none
 #returns: a json object with the html string containing options for all databases in the server.
 def loadDBList():
+    #connect to db to fetch master list
+    global conn
+    conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};'
+                      'SERVER=*;'
+                      'DATABASE=*;'
+                      'uid=*;'
+                      'pwd=*;')
     #create db query object
     print("Received DB list load request")
     db = conn.cursor()
@@ -46,34 +49,38 @@ def loadDBList():
 #param: name of database, username, and password for login
 #returns: nothing specifically but changes the instance of conn
 def newDB(dbName, user, pwd):
-    global conn
+    global userConn
     #close existing db connection
-    conn.close()
+    userConn.close()
     try:
-        conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};'
-                              'SERVER=*;'
-                              'DATABASE=' + str(dbName) + ';'
-                              'uid=' + str(user) + ';'
-                              'pwd=' + str(pwd) + ';')
+        userConn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};'
+                                  'SERVER=tech-web;'
+                                  'DATABASE=' + str(dbName) + ';'
+                                  'uid=' + str(user) + ';'
+                                  'pwd=' + str(pwd) + ';')
 
     except:
         return "INVALID CREDENTIAL"
 
-#used to find what table a query is searching
-#safety: unknown
-#param: query
-#returns: table from query
-def truncateQuery(query):
-    global curTable
-    curTable = query
-    tableEndInd = query.index(' FROM')
-    curTable = curTable[tableEndInd + 6:]
-    try:
-        curTable = curTable[:curTable.index(' WHERE')]
-    except:
-        print("No conditions specified")
-
-    return curTable
+#used to check for keywords that indicate actions of an sql injection. there's no need for any of these keywords, as the tool is intended to be read-only
+#param: a string containing a query or table
+#return: true if the statement passed in contains a flagged word, false otherwise
+#example: i try and enter a value into the value section that drops the current table. this is called before the query is executed, and will prevent it from executing
+def injectionAlert(tableString):
+    if("INSERT" in tableString):
+        return True
+    elif("DROP" in tableString):
+        return True
+    elif("DELETE" in tableString):
+        return True
+    elif("UPDATE" in tableString):
+        return True
+    elif("ALTER" in tableString):
+        return True
+    elif("CREATE" in tableString):
+        return True
+    else:
+        return False
 
 #used to load list of tables from current db for visual query tool
 #safety: safe from injection, does not take any input from the user.
@@ -83,11 +90,12 @@ def loadTableList():
     #create db query object
     print("Received table list load request")
     db = conn.cursor()
+    #execute query that fetches all tables from db
     sqlLoadTableList = "SELECT Distinct TABLE_NAME FROM information_schema.TABLES"
     db.execute(sqlLoadTableList)
     val = db.fetchall()
+    #put table list into html format for dropdown menu
     tableList = [x[0] for x in val]
-    #put table list into html for dropdown menu
     tableListString = ""
     for x in tableList:
         tableListString = tableListString + "<option value='" + x + "'>" + x + "</option>"
@@ -96,15 +104,15 @@ def loadTableList():
     return json.dumps(tableListString)
 
 #used to fetch a list of columns for the currently selected table in the visual query tool
-#safety: safe from injection, does not rely on user text input for feedback from the server. parameterized
+#safety: safe from injection, does not rely on user text input for feedback from the server, explicit checking
 #param: a table
 #returns: a json object with a string containing the html code for every column's name, where each column is in an <option> tag
 def loadColumnList(table):
     db = conn.cursor()
     tablePar = str(table)
-    if(("INSERT" in tablePar) or ("DROP" in tablePar) or ("DELETE" in tablePar) or ("UPDATE" in tablePar) or ("ALTER" in tablePar) or ("CREATE" in tablePar)):
-        return json.dumps("<option>Nice Try!</option>")
-        
+    #sql injection protection
+    if(injectionAlert(tablePar) or ("SELECT" in table)):
+        return json.dumps("<option>SQL Injection Detected</option>")
     sqlLoadColumnList = "SELECT * FROM ["+tablePar+"]"
     db.execute(sqlLoadColumnList)
     columnList = [str(column[0]) for column in db.description]
@@ -124,9 +132,9 @@ def loadColumnList(table):
 #returns: a string with the html code for a table holding the results
 def buildResultTableHTML(query):
     global curData, curColumns
-    temp = str(query)
     print("Received request to build result table")
-    if(("INSERT" in temp) or ("DROP" in temp) or ("DELETE" in temp) or ("UPDATE" in temp) or ("ALTER" in temp) or ("CREATE" in temp)):
+    #sql injection protection
+    if(injectionAlert(query)):
         result = "<tr><td>Query denied - contains keywords that signal an SQL Injection</tr></tr>"
         return json.dumps(result)
     #db cursor tool
@@ -163,6 +171,8 @@ def buildResultTableHTML(query):
 #safety: does not execute sql.
 #params: none explicitly, but it needs to be used after searching a table, or else the file will be empty
 #returns: a flask response containing the csv table
+
+
 def exportCSV():
     print("Request received to export CSV of results")
     si = StringIO()
